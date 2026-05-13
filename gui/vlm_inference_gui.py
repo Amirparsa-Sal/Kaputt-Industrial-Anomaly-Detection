f9b2af1b2e056696ade13f59bdc50d61ef1fe860
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2026-05-13 21:18:02 +0200

    fix slider bug in vlm_inference_gui

--- gui/vlm_inference_gui.py
+++ gui/vlm_inference_gui.py
@@ -538,7 +538,7 @@ def apply_filters(data_dir, split, is_defect, major_defect, defect_type, defect_
     if n == 0:
         return status, gr.update(minimum=0, maximum=1, value=0), gr.update(), None, None, ""
 
-    slider_update = gr.update(minimum=0, maximum=n - 1, value=0, step=1)
+    slider_update = gr.update(minimum=0, maximum=max(n - 1, 1), value=0, step=1)
     img_original, img_processed, info = _get_current_images(False, False, None)
     return status, slider_update, gr.update(value=f"1 / {n}"), img_original, img_processed, info
 
